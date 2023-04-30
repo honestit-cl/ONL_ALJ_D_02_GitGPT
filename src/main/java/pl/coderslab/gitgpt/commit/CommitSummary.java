f543cd085d56325cedfa3597db0e574827d59ca3
honestit <michal.kupisinski@gmail.com>
2023-04-30 12:37:20 +0200

    Add date formatting for commit summary

--- src/main/java/pl/coderslab/gitgpt/commit/CommitSummary.java
+++ src/main/java/pl/coderslab/gitgpt/commit/CommitSummary.java
@@ -1,6 +1,13 @@
 package pl.coderslab.gitgpt.commit;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDateTime;
 
 public record CommitSummary(
-    String sha, String author, String name, String destination, LocalDateTime date) {}
+    String sha,
+    String author,
+    String name,
+    String destination,
+    @JsonFormat(pattern = "eee MMM dd hh:mm:ss yyyy") LocalDateTime date) {}
