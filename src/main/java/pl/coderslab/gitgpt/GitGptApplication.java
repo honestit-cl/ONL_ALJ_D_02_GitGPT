e596d5ad2473e510435f4d2669ad9ce58010179c
honestit <michal.kupisinski@gmail.com>
2023-04-30 10:30:59 +0200

    Add initial project structure

---
+++ src/main/java/pl/coderslab/gitgpt/GitGptApplication.java
@@ -0,0 +1,12 @@
+package pl.coderslab.gitgpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitGptApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GitGptApplication.class, args);
+  }
+}
---
+++ src/test/java/pl/coderslab/gitgpt/GitGptApplicationTests.java
@@ -0,0 +1,11 @@
+package pl.coderslab.gitgpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitGptApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
